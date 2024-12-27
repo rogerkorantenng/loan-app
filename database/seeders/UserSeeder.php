383583487ae264ba0a01c9d7035b9221f6e69928
Bismark Yankson <guiderebo@gmail.com>
2024-12-27 14:13:10 +0000

    fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'name' => 'Roger Obeng',
             'email' => 'roger@gmail.com',
             'user_type' => 'admin',
-            'status' => 'active'
+            'status' => 'active',
             'password' => bcrypt('12345678')
             ]
         ]);
